fd55db2e9a6ddcbf89fdca0d0c04613daaec0400
Akshay Kamble <kamblea635@gmail.com>
2022-03-26 13:31:43 +0530

    create notespage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { LandingPage } from "./LandingPage/LandingPage";
 export { LoginPage } from "./LoginPage/LoginPage";
 export { SignupPage } from "./SignupPage/SignupPage";
+export { NotesPage } from "./NotesPage/NotesPage";
