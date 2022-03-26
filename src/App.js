9cc61efc509a592a08eb0fd955426b8497cff3b9
Akshay Kamble <kamblea635@gmail.com>
2022-03-26 14:01:14 +0530

    add route for notes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import MockmanEs from "mockman-js";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Footer, Navbar } from "./components";
-import { LandingPage, LoginPage, SignupPage } from "./pages/index";
+import { LandingPage, LoginPage, SignupPage, NotesPage } from "./pages/index";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/notes" element={<NotesPage />} />
         </Routes>
       </div>
       <Footer />
