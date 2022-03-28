4753f86d21f449ef6dd0f5f339b7e549f6293336
Akshay Kamble <kamblea635@gmail.com>
2022-03-28 18:55:13 +0530

    create add-note component

---
+++ src/components/AddNote/AddNote.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useNote } from "../../context/note-context";
+import "./AddNote.css";
+
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIwNGU2MDc4ZC0yMzFhLTQ4ZWMtOGQxOC00MzFlNTM4YjAwOGYiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ.j-V957JHq7BvOUWUg0FfkfZTbmaTHnlEb0DCyvYsDnA";
+export function AddNote(props) {
+  const { setAddNoteEnabled } = props;
+  const { notes, addNote, deleteNote } = useNote();
+  const [value, setValue] = useState({ title: "", text: "", date: "" });
+  const handlesubmit = (value) => {
+    addNote(value);
+    setValue({ title: "", text: "", date: "" });
+    setAddNoteEnabled(false);
+  };
+  console.log(value);
+  return (
+    <div className="note-container add-note">
+      <div className="note-text-area">
+        <textarea
+          onChange={(e) => setValue({ ...value, title: e.target.value })}
+          value={value.title}
+          className=" note-text note-title"
+          placeholder="Note title"
+        ></textarea>
+        <textarea
+          onChange={(e) => setValue({ ...value, text: e.target.value })}
+          value={value.text}
+          className="note-text text-area"
+          placeholder="Note"
+        ></textarea>
+      </div>
+      <div className="note-footer">
+        <div className="note-footer_left">
+          <span>
+            <input
+              onChange={(e) => setValue({ ...value, date: e.target.value })}
+              type="date"
+              name="date"
+              id="date"
+            />
+          </span>
+          <span
+            onClick={() => setAddNoteEnabled(false)}
+            className="material-icons-outlined note-action-button"
+          >
+            delete
+          </span>
+        </div>
+        <span
+          onClick={() => handlesubmit(value)}
+          className="material-icons-outlined btn-note-submit"
+        >
+          done
+        </span>
+      </div>
+    </div>
+  );
+}
