9f3d7f445878a55497a55576e9b44a31af1dffb1
Akshay Kamble <kamblea635@gmail.com>
2022-03-28 18:54:53 +0530

    Provided note-context

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import { NoteProvider } from "./context/note-context";
 
 // Call make Server
 makeServer();
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <NoteProvider>
+        <App />
+      </NoteProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
