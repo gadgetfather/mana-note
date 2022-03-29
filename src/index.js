b1741999b7fe138bed3168650230f7c43a8159a5
Akshay Kamble <kamblea635@gmail.com>
2022-03-29 10:38:16 +0530

    Provided auth context

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { NoteProvider } from "./context/note-context";
+import { AuthProvider } from "./context/auth-context";
 
 // Call make Server
 makeServer();
@@ -12,9 +13,11 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <NoteProvider>
-        <App />
-      </NoteProvider>
+      <AuthProvider>
+        <NoteProvider>
+          <App />
+        </NoteProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
