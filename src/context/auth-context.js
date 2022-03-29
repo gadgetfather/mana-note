35de35cdc44700b413896a6f29cb70b12407285c
Akshay Kamble <kamblea635@gmail.com>
2022-03-29 11:25:02 +0530

    add signup function

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 import react, { createContext, useContext, useReducer } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-const initialValue = { user: {}, status: "", login_error: "" };
+const initialValue = {
+  user: {},
+  status: "",
+  login_error: "",
+  signup_error: "",
+  text: "",
+  login_text: "",
+};
 const AuthContext = createContext();
 
 const authReducer = (state, action) => {
@@ -10,6 +17,12 @@ const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case "LOGIN_ERROR":
       return { ...state, login_error: action.payload };
+    case "SIGNUP_ERROR":
+      return { ...state, signup_error: action.payload };
+    case "SET_TEXT":
+      return { ...state, text: action.payload };
+    case "SET_LOGIN_TEXT":
+      return { ...state, login_text: action.payload };
 
     default:
       break;
@@ -31,15 +44,44 @@ const AuthProvider = ({ children }) => {
         localStorage.setItem("Mananote.Token", encodedToken);
         localStorage.setItem("Mananote.User", JSON.stringify(foundUser));
         authDispatch({ type: "SET_USER", payload: foundUser });
-        navigate("/notes");
+        authDispatch({
+          type: "SET_LOGIN_TEXT",
+          payload: "Logged in successfully ",
+        });
+        setTimeout(() => {
+          authDispatch({ type: "SET_TEXT", payload: "" });
+          navigate("/notes");
+        }, 2000);
       }
     } catch (error) {
       authDispatch({ type: "LOGIN_ERROR", payload: error });
     }
   };
 
+  const signup = async (email, password, firstName) => {
+    try {
+      authDispatch({ type: "SIGNUP_ERROR", payload: "" });
+      const response = await axios.post("/api/auth/signup", {
+        email,
+        password,
+        firstName,
+      });
+      const { status } = response;
+      if (status === 201) {
+        authDispatch({ type: "SET_TEXT", payload: "Account has been created" });
+        setTimeout(() => {
+          authDispatch({ type: "SET_TEXT", payload: "" });
+          navigate("/login");
+        }, 2000);
+      }
+      console.log(response);
+    } catch (error) {
+      authDispatch({ type: "SIGNUP_ERROR", payload: error });
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ authInfo, login }}>
+    <AuthContext.Provider value={{ authInfo, login, signup }}>
       {children}
     </AuthContext.Provider>
   );
