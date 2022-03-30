22f5fe61ea34a075878a1f555e844e2ba5a43fac
Akshay Kamble <kamblea635@gmail.com>
2022-03-30 19:58:44 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Footer } from "./Footer/Footer";
 export { Note } from "./Note/Note";
 export { AddNote } from "./AddNote/AddNote";
 export { OptionMenu } from "./OptionMenu/OptionMenu";
+export { Sidebar } from "./Sidebar/Sidebar";
