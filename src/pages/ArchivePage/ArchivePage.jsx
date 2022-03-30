b6a949f17b0f1b7e937a96e152d380dd1d75f2a1
Akshay Kamble <kamblea635@gmail.com>
2022-03-30 12:21:18 +0530

    Create ArchivePage.jsx

---
+++ src/pages/ArchivePage/ArchivePage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Note, OptionMenu } from "../../components";
+import { useArchive } from "../../context/archive-context";
+
+export function ArchivePage() {
+  const { archivesArr } = useArchive();
+  return (
+    <main className="main-content_notes">
+      <div className="searchbar-container-desktop">
+        <input
+          placeholder="Search for a note..."
+          className="note-searchbar-desktop"
+          type="text"
+        />
+        <span className="material-icons sort-icon-desktop">filter_list</span>
+      </div>
+      <div className="searchbar-container-mobile">
+        <input className="note-searchbar-mobile" type="text" />
+        <span className="material-icons sort-icon">category</span>
+      </div>
+      <h2 className="page-title">Archive</h2>
+      <div className="notes-list">
+        {archivesArr.map((item) => (
+          <Note key={item._id} {...item} />
+        ))}
+      </div>
+
+      <button
+        // onClick={handleAddNote}
+        className="btn btn-floating note-floating-btn"
+      >
+        <span className="material-icons-outlined">add</span>
+      </button>
+      <OptionMenu />
+    </main>
+  );
+}
