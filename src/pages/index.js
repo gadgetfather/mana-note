f536d1d4138438cf4ce514d70c5eeb572e43bcc6
Akshay Kamble <kamblea635@gmail.com>
2022-03-31 22:32:29 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { SignupPage } from "./SignupPage/SignupPage";
 export { NotesPage } from "./NotesPage/NotesPage";
 export { ArchivePage } from "./ArchivePage/ArchivePage";
 export { InfoPage } from "./InfoPage/InfoPage";
+export { TrashPage } from "./TrashPage/TrashPage";
