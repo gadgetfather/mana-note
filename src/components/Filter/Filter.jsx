2a1328b0b4b88ce1182cfc6fb667d96ae2828c73
Akshay Kamble <kamblea635@gmail.com>
2022-04-01 21:54:51 +0530

    Create mobile filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useFilter } from "../../context/filter-context";
 import "./Filter.css";
+
 export function Filter() {
+  const {
+    filter: { SortTime, SortPriority },
+    filterDispatch,
+  } = useFilter();
   return (
     <div className="filter-container">
       <div className="filter-title-container">
@@ -11,22 +17,54 @@ export function Filter() {
       <h2>By Time:</h2>
       <div className="sort-container">
         <span>
-          <input name="byTime" id="newToOld" type="radio" />
+          <input
+            checked={SortTime && SortTime === "NEW_TO_OLD"}
+            onChange={() =>
+              filterDispatch({ type: "NEW_TO_OLD", payload: "NEW_TO_OLD" })
+            }
+            name="byTime"
+            id="newToOld"
+            type="radio"
+          />
           <label htmlFor="newToOld">New to old</label>
         </span>
         <span>
-          <input name="byTime" id="oldToNew" type="radio" />
+          <input
+            checked={SortTime && SortTime === "OLD_TO_NEW"}
+            onChange={() =>
+              filterDispatch({ type: "OLD_TO_NEW", payload: "OLD_TO_NEW" })
+            }
+            name="byTime"
+            id="oldToNew"
+            type="radio"
+          />
           <label htmlFor="oldToNew">Old to New</label>
         </span>
       </div>
       <h2>By priority:</h2>
       <div className="sort-container">
         <span>
-          <input name="byPriority" id="highToLow" type="radio" />
+          <input
+            checked={SortPriority && SortPriority === "HIGH_TO_LOW"}
+            onChange={() =>
+              filterDispatch({ type: "HIGH_TO_LOW", payload: "HIGH_TO_LOW" })
+            }
+            name="byPriority"
+            id="highToLow"
+            type="radio"
+          />
           <label htmlFor="highToLow">High to low</label>
         </span>
         <span>
-          <input name="byPriority" id="lowToHigh" type="radio" />
+          <input
+            checked={SortPriority && SortPriority == "LOW_TO_HIGH"}
+            onChange={() =>
+              filterDispatch({ type: "LOW_TO_HIGH", payload: "LOW_TO_HIGH" })
+            }
+            name="byPriority"
+            id="lowToHigh"
+            type="radio"
+          />
           <label htmlFor="lowToHigh">Low to High</label>
         </span>
       </div>
