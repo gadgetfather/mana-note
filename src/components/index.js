8ca632be856978089478206b7e7337247956e3cd
Akshay Kamble <kamblea635@gmail.com>
2022-04-01 21:54:23 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,5 @@ export { OptionMenu } from "./OptionMenu/OptionMenu";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { TrashNote } from "./TrashNote/TrashNote";
 export { Filter } from "./Filter/Filter";
+export { DesktopFilter } from "./DesktopFilter/DesktopFilter";
+export { EditNote } from "./EditNote/EditNote";
