c0ed3b4d87da3c58abdf11c37d191380a742aca8
Akshay Kamble <kamblea635@gmail.com>
2022-04-01 15:29:08 +0530

    Update note page to have filter

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import "./NotesPage.css";
-import { AddNote, Note, OptionMenu, Sidebar } from "../../components";
+import { AddNote, Filter, Note, OptionMenu, Sidebar } from "../../components";
 import { useNote } from "../../context/note-context";
+import { useFilter } from "../../context/filter-context";
 export function NotesPage() {
+  const [filterOpen, setFilterOpen] = useState(false);
   const { notes } = useNote();
   const [addNoteEnabled, setAddNoteEnabled] = useState(false);
+
   const handleAddNote = () => {
     setAddNoteEnabled(true);
   };
+
   return (
     <main className="main-content_notes">
       <div className="searchbar-container-desktop">
@@ -21,8 +25,14 @@ export function NotesPage() {
       </div>
       <div className="searchbar-container-mobile">
         <input className="note-searchbar-mobile" type="text" />
-        <span className="material-icons sort-icon">category</span>
+        <span
+          onClick={() => setFilterOpen(!filterOpen)}
+          className="material-icons sort-icon"
+        >
+          category
+        </span>
       </div>
+      {filterOpen ? <Filter /> : ""}
       <div className="content-container">
         <Sidebar />
         <div className="content-container-right">
