8ca6a98b3820f226184459377c7727aa057836f8
Akshay Kamble <kamblea635@gmail.com>
2022-04-04 21:18:28 +0530

    update siderbar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,47 @@
 import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import "./Sidebar.css";
 export function Sidebar() {
   const location = useLocation();
   const [showInfo, setShowInfo] = useState(false);
   return (
     <aside className="sidebar">
-      <Link
-        className={
-          location.pathname == "/notes"
-            ? "link sidebar-item sidebar-active"
-            : "link sidebar-item "
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? "link sidebar-item sidebar-active" : "link sidebar-item "
         }
         to="/notes"
       >
         <span className="material-icons-outlined">notes</span>
         <span>Notes</span>
-      </Link>
-      <Link className="link sidebar-item" to="/label">
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? "link sidebar-item sidebar-active" : "link sidebar-item "
+        }
+        to="/label"
+      >
         <span className="material-icons-outlined">label</span>
         <span>Label</span>
-      </Link>
-      <Link className="link sidebar-item" to="/trash">
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? "link sidebar-item sidebar-active" : "link sidebar-item "
+        }
+        to="/trash"
+      >
         <span className="material-icons-outlined">delete</span>
         <span>Trash</span>
-      </Link>
-      <Link
-        className={
-          location.pathname == "/archived"
-            ? "link sidebar-item sidebar-active"
-            : "link sidebar-item "
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? "link sidebar-item sidebar-active" : "link sidebar-item "
         }
         to="/archived"
       >
         <span className="material-icons-outlined">archive</span>
         <span>Archive</span>
-      </Link>
+      </NavLink>
       <div
         className={`info-container ${showInfo ? "info-container-show" : ""} `}
       >
